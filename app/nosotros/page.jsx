44980eead25ad8c56c5ad8cfb236e6eff7fc8dde
crbzxy <carlos.boor@gmail.com>
2023-05-07 02:38:10 -0600

    add

---
+++ app/nosotros/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function pageNosotros() {
+  return (
+    <div>
+      <header>
+        <h1>Hola mundo</h1>
+      </header>
+      <section>
+        <h2>Section 2</h2>
+      </section>
+    </div>
+  )
+}
+
+export default pageNosotros
